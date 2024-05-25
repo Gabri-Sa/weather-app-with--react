f2586a7890a291d0f7b298dc75ae8fd9b8a1699e
Gabri-Sa <158731669+Gabri-Sa@users.noreply.github.com>
2024-05-25 18:53:17 +0100

    added Bootstrap

--- src/App.js
+++ src/App.js
@@ -5,16 +5,20 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App - with React</h1>
-      <footer>
-        This project was coded by <em>Gabriela Saftoiu</em> and is open-sourced
-        <a
-          href="https://github.com/Gabri-Sa/weather-app-with--react"
-          targer="_blank"
-        >
-          on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App - with React</h1>
+        <footer>
+          This project was coded by <em>Gabriela Saftoiu</em> and is
+          open-sourced on
+          <a
+            href="https://github.com/Gabri-Sa/weather-app-with--react"
+            target="_blank"
+          >
+            {" "}
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
