a89acee7bbe0b1961137684c7c6310be524ee5ab
Gabri-Sa <158731669+Gabri-Sa@users.noreply.github.com>
2024-05-25 14:26:16 +0100

    Added footer

--- src/App.js
+++ src/App.js
@@ -3,5 +3,18 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App"> Weather App in React </div>;
+  return (
+    <div className="App">
+      Weather App - with React
+      <footer>
+        This project was coded by <em>Gabriela Saftoiu</em> and is open-sourced
+        <a
+          href="https://github.com/Gabri-Sa/weather-app-with--react"
+          targer="_blank"
+        >
+          on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
