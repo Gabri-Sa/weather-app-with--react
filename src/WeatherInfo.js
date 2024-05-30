8b842d16edda46e22faff944092818ec926d4075
Gabri-Sa <158731669+Gabri-Sa@users.noreply.github.com>
2024-05-30 21:26:42 +0100

    Adjusted temperature units

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -14,9 +14,15 @@ export default function WeatherInfo(props) {
         <li className="text-capitalize">{props.data.description}</li>
       </ul>
       <div className="row mt-3">
-        <div className="col-6 d-flex">
-          <WeatherIcon code={props.data.icon} size={52} />
-          <WeatherTemperature celsius={props.data.temperature} />
+        <div className="col-6">
+          <div className="clearfix">
+            <div className="float-left">
+              <WeatherIcon code={props.data.icon} size={52} />
+            </div>
+            <div className="float-left">
+              <WeatherTemperature celsius={props.data.temperature} />
+            </div>
+          </div>
         </div>
         <div className="col-6">
           <ul>
